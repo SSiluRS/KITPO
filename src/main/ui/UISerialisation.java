c3ac612243abf0b27b2d798c2ad6001186477d2d
SSiluRS <Endercresman397@mail.ru>
2022-10-21 16:09:16 +0700

    File saving changes

--- src/main/ui/UISerialisation.java
+++ src/main/ui/UISerialisation.java
@@ -3,30 +3,39 @@ package main.ui;
 import main.data.IList;
 import main.data.builder.UserTypeBuilder;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class UISerialisation {
 
     public static  void saveToFile(String filename, IList list, UserTypeBuilder builder) throws FileNotFoundException {
-        try (PrintWriter writer = new PrintWriter(filename)) {
-            writer.println(builder.typeName());
-            list.forEach(el -> writer.println(builder.toString(el)));
+        try (FileOutputStream fos = new FileOutputStream(filename)){
+            ObjectOutputStream out = new ObjectOutputStream(fos);
+            out.writeObject(builder.typeName());
+            out.writeObject(list.size());
+            list.forEach(el -> {
+                try {
+                    out.writeObject(builder.toString(el));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public static  IList loadFromFile(String filename, UserTypeBuilder builder, IList list) throws Exception {
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
             String line;
 
-            line = br.readLine();
+            line = (String) (in.readObject());
             if (!builder.typeName().equals(line)) {
                 throw new Exception("Wrong file structure");
             }
-
-            while ((line = br.readLine()) != null) {
+            var size = (Integer)in.readObject();
+            for (int i = 0; i < size; i++) {
+                line = (String)in.readObject();
                 list.add(builder.createFromString(line));
             }
             return list;
