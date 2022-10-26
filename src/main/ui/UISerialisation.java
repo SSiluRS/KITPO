dab1e720971f042760f8584fc13434f875aa152d
SSiluRS <Endercresman397@mail.ru>
2022-10-26 18:38:34 +0700

    Small changes

--- src/main/ui/UISerialisation.java
+++ src/main/ui/UISerialisation.java
@@ -8,34 +8,30 @@ import java.io.*;
 public class UISerialisation {
 
     public static  void saveToFile(String filename, IList list, UserTypeBuilder builder) throws FileNotFoundException {
-        try (FileOutputStream fos = new FileOutputStream(filename)){
-            ObjectOutputStream out = new ObjectOutputStream(fos);
-            out.writeObject(builder.typeName());
-            out.writeObject(list.size());
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(builder.typeName()+"\n");
             list.forEach(el -> {
                 try {
-                    out.writeObject(builder.toString(el));
+                    writer.write(builder.toString(el)+"\n");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
-            out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static  IList loadFromFile(String filename, UserTypeBuilder builder, IList list) throws Exception {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
 
-            line = (String) (in.readObject());
+            line = br.readLine();
             if (!builder.typeName().equals(line)) {
                 throw new Exception("Wrong file structure");
             }
-            var size = (Integer)in.readObject();
-            for (int i = 0; i < size; i++) {
-                line = (String)in.readObject();
+
+            while ((line = br.readLine()) != null) {
                 list.add(builder.createFromString(line));
             }
             return list;
